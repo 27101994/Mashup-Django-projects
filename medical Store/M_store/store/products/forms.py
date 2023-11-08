2b411290fdbefc8368f51227bb40c2dea51c64a3
Aravind s s <114403185+27101994@users.noreply.github.com>
2023-11-08 11:48:52 +0530

    Add files via upload

---
+++ medical Store/M_store/store/products/forms.py
@@ -0,0 +1,25 @@
+
+
+from django import forms
+from .models import Products
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import Customer
+from django.contrib.auth.models import User
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Products
+        fields = '__all__'
+
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')  # Include additional fields as needed
+
+
+
+ 
